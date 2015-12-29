373fda9ab3cd3ab440f002f59a3f741ad3410e23
PolGuixe <pol.guixe@gmail.com>
2015-12-29 16:35:54 +0100

    release 0.1.0
    implemented package

--- package.js
+++ package.js
@@ -2,23 +2,19 @@ Package.describe({
   name: 'polguixe:meteor-clipperjs',
   version: '6.1.3a',
   // Brief, one-line summary of the package.
-  summary: '',
+  summary: 'ClipperJs library wrapped for Meteor',
   // URL to the Git repository containing the source code for this package.
-  git: '',
+  git: 'https://github.com/PolGuixe/meteor-clipperjs',
   // By default, Meteor will default to using README.md for documentation.
   // To avoid submitting documentation, set this field to null.
   documentation: 'README.md'
 });
 
 Package.onUse(function(api) {
+//Dependencies
   api.versionsFrom('1.2.1');
-  api.use('ecmascript');
-  api.addFiles('meteor-clipperjs.js');
-});
 
-Package.onTest(function(api) {
-  api.use('ecmascript');
-  api.use('tinytest');
-  api.use('polguixe:meteor-clipperjs');
-  api.addFiles('meteor-clipperjs-tests.js');
+  //Main Library
+  api.addFiles('lib/clipper.js');
+  api.export('ClipperLib');
 });
